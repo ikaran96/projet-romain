7ab8dfd904613b6acfc03a6fee92fbbb303ecba3
ikaran96 <ilaydakaran@yahoo.fr>
2020-04-15 15:57:58 +0200

    FOOTER

---
+++ footer.php
@@ -0,0 +1,81 @@
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/reset.css">
+    <link rel="stylesheet" href="css/var.css">
+    <link rel="stylesheet" href="css/style.css">
+    <!--Polices Google-->
+    <link
+        href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&family=Roboto+Slab:wght@100;200;300;400;500;600;700;800;900&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap"
+        rel="stylesheet">
+    <title>Le Bon Sens Français</title>
+</head>
+<footer>
+    <!--Premier footer-->
+    <div class="footer-1">
+        <div class="footer-col">
+            <p class="col-title">CATÉGORIES DE LOSES</p>
+
+            <ul class="list-footer">
+                <li class="item-footer"><a class="footer-url-bg" href="">LOSERS DU WEEKEND</a></li>
+                <li class="item-footer"><a class="footer-url-bg" href="">LOSERS DU WEEKEND</a></li>
+                <li class="item-footer"><a class="footer-url-bg" href="">LOSERS DU WEEKEND</a></li>
+                <li class="item-footer"><a class="footer-url-bg" href="">LOSERS DU WEEKEND</a></li>
+                <li class="item-footer"><a class="footer-url-bg" href="">LOSERS DU WEEKEND</a></li>
+                <li class="item-footer"><a class="footer-url-bg" href="">LOSERS DU WEEKEND</a></li>
+                <li class="item-footer"><a class="footer-url-bg" href="">LOSERS DU WEEKEND</a></li>
+                <li class="item-footer"><a class="footer-url-bg" href="">LOSERS DU WEEKEND</a></li>
+                <li class="item-footer"><a class="footer-url-bg" href="">LOSERS DU WEEKEND</a></li>
+                <li class="item-footer"><a class="footer-url-bg" href="">LOSERS DU WEEKEND</a></li>
+                <li class="item-footer"><a class="footer-url-bg" href="">LOSERS DU WEEKEND</a></li>
+                <li class="item-footer"><a class="footer-url-bg" href="">LOSERS DU WEEKEND</a></li>
+                <li class="item-footer"><a class="footer-url-bg" href="">LOSERS DU WEEKEND</a></li>
+                <li class="item-footer"><a class="footer-url-bg" href="">LOSERS DU WEEKEND</a></li>
+                <li class="item-footer"><a class="footer-url-bg" href="">LOSERS DU WEEKEND</a></li>
+                <li class="item-footer"><a class="footer-url-bg" href="">LOSERS DU WEEKEND</a></li>
+            </ul>
+        </div>
+
+        <div class="footer-col">
+            <p class="col-title">DERNIERS ARTICLES</p>
+            <ul class="list-footer-articles">
+                <li class="item-footer-articles"><a class="footer-url" href="">Le Quiz FFL | Qui a joué en Ligue des
+                        Champions ? Montpellier HSC Edition</a></li>
+                <li class="item-footer-articles"><a class="footer-url" href="">Le Quiz FFL | Qui a joué en Ligue des
+                        Champions ? Montpellier HSC Edition</a></li>
+                <li class="item-footer-articles"><a class="footer-url" href="">Le Quiz FFL | Qui a joué en Ligue des
+                        Champions ? Montpellier HSC Edition</a></li>
+                <li class="item-footer-articles"><a class="footer-url" href="">Le Quiz FFL | Qui a joué en Ligue des
+                        Champions ? Montpellier HSC Edition</a></li>
+
+            </ul>
+        </div>
+
+        <div class="footer-col">
+            <p class="col-title">INFORMATIONS ET CONTACT</p>
+            <ul class="list-footer">
+                <li class="item-footer"><a class="footer-url-bg" href="">MON COMPTE</a></li>
+                <li class="item-footer"><a class="footer-url-bg" href="">MON COMPTE</a></li>
+                <li class="item-footer"><a class="footer-url-bg" href="">MON COMPTE</a></li>
+                <li class="item-footer"><a class="footer-url-bg" href="">MON COMPTE</a></li>
+            </ul>
+        </div>
+    </div>
+
+    <!--Deuxième footer-->
+
+    <div class="footer-2">
+        <div class="bottom-footer">
+            <a class="bottom-footer-url" href="">Lien</a>
+            <a class="bottom-footer-url" href="">Lien</a>
+            <a class="bottom-footer-url" href="">Lien</a>
+        </div>
+        <div class="copyright-footer">
+            <p class="copyright">© 2020 FFL Média | tous droits réservés</p>
+        </div>
+    </div>
+</footer>
+
+</html>
---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/reset.css">
+    <link rel="stylesheet" href="css/var.css">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Le Bon Sens Français</title>
+
+</head>
+
+<body>
+
+    <?php include 'include/nav.php'; ?>
+
+
+    <?php include 'include/footer.php'; ?>
+    
+</body>
+
+</html>
