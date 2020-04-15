d601d5c5dfd536ba9d76b80a43080b0ad3f20e3b
christophesd <christophe.sd@gmail.com>
2020-04-15 16:36:39 +0200

    articles cards

--- index.php
+++ index.php
@@ -5,10 +5,17 @@
 
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&family=Roboto+Slab:wght@100;200;300;400;500;600;700;800;900&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
+
     <link rel="stylesheet" href="css/reset.css">
     <link rel="stylesheet" href="css/var.css">
     <link rel="stylesheet" href="css/style.css">
-    <title>Le Bon Sens Français</title>
+
+
+
+
+    <title>Le Bon Sens Français - Accueil</title>
 
 </head>
 
@@ -18,16 +25,85 @@
     
     <div id="page">
 
-        <div class="bar">
+        <main id="article">
 
-        </div>
+            <div class="card-article">
 
-        <main>
+                <a href=""><h2 class="title">Le Quiz FFL | Paris Roubaix – Les vainqueurs et meilleurs français depuis 1997.</h2></a>
 
+                <h3 class="subtitle">Si sous les pavés, il y a la plage, sur les pavés, c’est plutôt l’enfer. La plus belle des classiques a été annulée cette année...</h3>
+
+                <div class="img">
+                    <a href=""><img class="logo" src="https://fflose.com/wp-content/uploads/2017/10/Picto-Cyclisme.svg" alt=""></a>
+                    <a href=""><img class="image" src="https://cdn.shortpixel.ai/client/q_glossy,ret_img,w_1200/https://fflose.com/wp-content/uploads/2020/04/12042020-Paris-Roubaix.jpg" alt=""></a>
+                </div>
+
+                <div class="category">
+                    <a href="#">CYCLISME</a> 
+                    <a href="#">QUIZ SPORT</a>
+                </div>
+
+            </div>
+
+            <div class="card-article">
+
+                <a href=""><h2 class="title">Le Quiz FFL | Qui a joué en Ligue des Champions ? RC Lens Edition</h2></a>
+
+                <h3 class="subtitle">Ils ont foulé la pelouse sous l’hymne de La Ligue des Champions. Ils ont eu cette chance que tu n’as pu vivre que dans tes...</h3>
+
+                <div class="img">
+                    <a href=""><img class="logo" src="https://fflose.com/wp-content/uploads/2017/10/Picto-Football.svg" alt=""></a>
+                    <a href=""><img class="image" src="https://cdn.shortpixel.ai/client/q_glossy,ret_img,w_1200/https://fflose.com/wp-content/uploads/2020/04/13042020-Lens-EN-C1.jpg" alt=""></a>
+                </div>
+
+                <div class="category">
+                    <a href="#">FOOTBALL</a> 
+                    <a href="#">QUIZ SPORT</a>
+                </div>
+
+            </div>
+
+            <div class="card-article">
+
+                <a href=""><h2 class="title">Top 10 des plus beaux ratés des parieurs sportifs</h2></a>
+
+                <h3 class="subtitle">Le pari sportif est un monde de montagnes russes émotionnelles. On pense parfois gagner le gros lot en passant un combiné de plusieurs matchs, puis...</h3>
+
+                <div class="img">
+                    <a href=""><img class="logo" src="https://fflose.com/wp-content/uploads/2017/10/Picto-Autres.svg" alt=""></a>
+                    <a href=""><img class="image" src="https://cdn.shortpixel.ai/client/q_glossy,ret_img,w_880/https://fflose.com/wp-content/uploads/2020/04/paris-FFL.png" alt=""></a>
+                </div>
+
+                <div class="category">
+                    <a href="#">TOP</a> 
+                </div>
+
+            </div>
 
         </main>
 
         <aside>
+
+            <div class="fb-page" data-href="https://www.facebook.com/FFLose/" data-tabs="timeline" data-width="" data-height="" data-small-header="false" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true">
+                <blockquote cite="https://www.facebook.com/FFLose/" class="fb-xfbml-parse-ignore"> 
+                    <a href="https://www.facebook.com/FFLose/">FFL - Fédération Française de la Lose</a>
+                </blockquote>
+            </div>
+
+            <div class="newsletter">
+
+                <span class="title">NEWSLETTER</span>
+                <span class="subtitle">Recevez les dernières actualités</span>
+                <span class="action">inscrivez-vous vite</span>
+
+                <form action="">
+                    <input type="text" name="" id="">
+                    <button>JE M'INSCRIS</button>
+                </form>
+
+                <span class="nospan">Vous ne recevrez aucun SPAM</span>
+
+            </div>
 
 
         </aside>
@@ -37,6 +113,9 @@
 
     <?php include 'include/footer.php'; ?>
     
+    <div id="fb-root"></div>
+    <script async defer crossorigin="anonymous" src="https://connect.facebook.net/fr_FR/sdk.js#xfbml=1&version=v6.0"></script>
+
 </body>
 
 </html>
