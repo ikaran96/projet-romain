3e44524586e508f238571b68f0178bdbb5fb8eb7
guillaumepoucet <poucet@simplon-charleville.fr>
2020-04-16 16:28:00 +0200

    css ok

--- top-article.php
+++ top-article.php
@@ -27,23 +27,25 @@
 
         <main>
 
-            <a class="tag" href="">Autres</a>
-
-            <h3 class="top-art-title">Top 10 des réponses des sportifs français à la FFL</h3>
-
-            <div class="top-card-img" href="top-article.php">
-                <img src="assests\img\best-of-humour-ffl-sportifs.png" alt="">
-            </div>
-
-            <p class="date">2 mois</p>
+            <a class="tag tag-art" href="">Autres</a>
 
             <article>
+
+                <h3 class="top-art-title title-art">Top 10 des réponses des sportifs français à la FFL</h3>
+
+                <p class="date">2 mois</p>
+
+                <div class="top-card-img img-art" href="top-article.php">
+                    <img src="assests\img\best-of-humour-ffl-sportifs.png" alt="">
+                </div>
 
                 <p class="intro">
                     Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio ipsam at consequatur tenetur
                     accusamus tempore ut, similique necessitatibus, cupiditate minus nulla reprehenderit, veritatis
                     eaque vitae quae error iure. Perferendis, illo.
                 </p>
+
+                <h4>Lorem ipsum dolor sit amet</h4>
 
                 <p>
                     Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione quisquam cum nostrum ea
@@ -54,7 +56,9 @@
                     sint id!
                 </p>
 
-                <h4>Lorem ipsum dolor sit amet</h4>
+                <div class="similar-content">
+                    <p>À lire également : <a href="">Lien bon sens français</a></p>
+                </div>
 
             </article>
 
