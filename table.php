ac197f10e8340598e22029d52bfda95c5d4cca7b
guillaumepoucet <poucet@simplon-charleville.fr>
2020-04-21 19:18:34 +0200

    21/04/20

---
+++ table.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link
+        href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&family=Roboto+Slab:wght@100;200;300;400;500;600;700;800;900&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap"
+        rel="stylesheet">
+
+    <link rel="stylesheet" href="css/reset.css">
+    <link rel="stylesheet" href="css/var.css">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/nav.css">
+    <link rel="stylesheet" href="css/table.css">
+
+    <script src="assests/font-awesome/all.min.js"></script>
+
+    <title>Le Bon Sens Français - Accueil</title>
+
+</head>
+
+<body>
+
+    <div id="admin-art-list">
+
+        <div class="admin-art-list-title">
+
+            <div>
+                <img src="assests\logo\Icon awesome-newspaper.svg" alt="">
+                <h2>Gestion des articles</h2>
+            </div>
+
+            <form>
+                
+                <div class="search-bar">
+                    <input type="text">
+                    <img src="assests/logo/Icon awesome-search.svg" alt="">
+                </div>
+
+                <div class="custom-select">
+                    <select>
+                        <option value="0">Catégories</option>
+                        <option value="1">Baguette</option>
+                    </select>
+                </div>
+
+            </form>
+
+        </div>
+
+    </div>
+
+</body>
+
+</html>
