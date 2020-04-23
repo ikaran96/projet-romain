0c5e668e8f0632c0a20d656d1bfcd018a65715ab
ikaran96 <ilaydakaran@yahoo.fr>
2020-04-23 16:55:12 +0200

    articles presque done

--- include/footer.php
+++ include/footer.php
@@ -1,18 +1,4 @@
-<html>
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css/reset.css">
-    <link rel="stylesheet" href="css/var.css">
-    <link rel="stylesheet" href="css/style.css">
-    <!--Polices Google-->
-    <link
-        href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&family=Roboto+Slab:wght@100;200;300;400;500;600;700;800;900&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap"
-        rel="stylesheet">
-    <title>Le Bon Sens Français</title>
-</head>
-<footer>
+<footer id="footer">
     <!--Premier footer-->
     <div class="footer-1">
         <div class="footer-col">
@@ -56,7 +42,7 @@
         <div class="footer-col">
             <p class="col-title">INFORMATIONS ET CONTACT</p>
             <ul class="list-footer">
-                <li class="item-footer"><a class="footer-url-bg" href="">MON COMPTE</a></li>
+                <li class="item-footer"><a class="footer-url-bg" href="index.php?p=contact">NOUS CONTACTER</a></li>
                 <li class="item-footer"><a class="footer-url-bg" href="">MON COMPTE</a></li>
                 <li class="item-footer"><a class="footer-url-bg" href="">MON COMPTE</a></li>
                 <li class="item-footer"><a class="footer-url-bg" href="">MON COMPTE</a></li>
@@ -77,5 +63,3 @@
         </div>
     </div>
 </footer>
-
-</html>
