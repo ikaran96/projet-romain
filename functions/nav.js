dc77b2f003af2d7112a263fa316ba6c33332ec8f
guillaumepoucet <poucet@simplon-charleville.fr>
2020-05-05 14:58:37 +0200

    supp

--- functions/nav.js
+++
@@ -1,9 +0,0 @@
-/* Set the width of the side navigation to 250px */
-function openNav() {
-    document.getElementById("myNav").style.width = "100%";
-}
-
-/* Set the width of the side navigation to 0 */
-function closeNav() {
-    document.getElementById("myNav").style.width = "0";
-}
