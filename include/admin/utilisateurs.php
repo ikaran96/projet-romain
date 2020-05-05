5e89fe01f351a2034ebc4086af4c5254fcb4317a
guillaumepoucet <poucet@simplon-charleville.fr>
2020-05-05 14:46:18 +0200

    dernier push

--- include/admin/utilisateurs.php
+++ include/admin/utilisateurs.php
@@ -26,6 +26,8 @@
                     <th class="column3">Prénom</th>
                     <th class="column4">Mail</th>
                     <th class="column5">date d'inscription</th>
+                    <th class="column7"></th>
+                    <th class="column8"></th>
                 </tr>
             </thead>
             <tbody>
@@ -35,6 +37,8 @@
                     <td class="column3">Jean-Pierre</td>
                     <td class="column4">jean-pierre@richard.fr</td>
                     <td class="column5">12/12/2020</td>
+                    <td class="column7"><a>Éditer</a></td>
+                    <td class="column8"><a>Supprimer</a></td>
                 </tr>
             </tbody>
         </table>
